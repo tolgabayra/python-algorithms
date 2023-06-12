64f3d1f1b082e09212806c87200d51bcc2b78093
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-06-12 18:54:34 +0300

    binary search algorithms

---
+++ src/binary_search/main.py
@@ -0,0 +1,63 @@
+from numpy import random
+
+
+def BinarySearch(array, x, low, high):
+    while low <= high:
+        mid = low + (high - low) // 2
+
+        if array[mid] == x:
+            return mid
+        elif array[mid] < x:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+
+array = [3, 4, 5, 6, 7, 8, 9]
+x = 9
+
+result = BinarySearch(array, x, 0, len(array) - 1)
+
+if result != -1:
+    print("Element is present at index " + str(result))
+else:
+    print("Not found")
+
+
+"""
+
+
+# Binary Search in python(recursive)
+def binarySearch(array, x, low, high):
+    if high >= low:
+        mid = low + (high - low) // 2
+
+        # If found at mid, then return it
+        if array[mid] == x:
+            return mid
+
+        # Search the left half
+        elif array[mid] > x:
+            return binarySearch(array, x, low, mid - 1)
+
+        # Search the right half
+        else:
+            return binarySearch(array, x, mid + 1, high)
+
+    else:
+        return -1
+
+
+array = [3, 4, 5, 6, 7, 8, 9]
+x = 4
+
+result = binarySearch(array, x, 0, len(array) - 1)
+
+if result != -1:
+    print("Element is present at index " + str(result))
+else:
+    print("Not found")
+
+
+"""
