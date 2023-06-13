b82977b03babc35d91f290d27d3af259a7ee42e4
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-06-13 14:47:35 +0300

    insertion sort added

---
+++ src/insertion_sort/main.py
@@ -0,0 +1,14 @@
+def InsertionSort(array):
+    for step in range(1, len(array)):
+        key = array[step]
+        j = step - 1
+        while j >= 0 and key < array[j]:
+            array[j + 1] = array[j]
+            j = j - 1
+        array[j + 1] = key
+
+
+array = [9, 5, 1, 4, 3]
+InsertionSort(array)
+print("Sorted Array in Ascending Order:")
+print(array)
